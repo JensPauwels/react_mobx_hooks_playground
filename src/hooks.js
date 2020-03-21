3e85e0ec53c0e582cfd7a8c9bebb0ed57876f2b5
Jens Pauwels <jenspauwels@Jenss-MBP-3.home>
2020-03-21 19:09:17 +0100

    updated

--- src/hooks.js
+++ src/hooks.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { storeContext } from '../context';
+import { storeContext } from './context';
 
 export const useStore = store => {
   return useContext(storeContext)[store];
